0dd22eee50a5d80fb292602eff3bf60c83ff8454
Aidan <aidanyoung237@gmail.com>
2026-02-16 11:26:00 -0600

    Initial test code for creating a window

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# Write code here
+import sys
+from PyQt6.Widgets import QApplication, QWidget
+
+app = QApplication(sys.argv)
+
+main_window = QWidget()
+main_window.setWindowTitle("Twist & Tune")
+main_window.setGeometry(100, 100, 800, 600)
+main_window.show()
+
+sys.exit(app.exec())
